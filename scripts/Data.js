38a1241e7bae785876182bd2f5bcbe6e5e45140e
Ali Developer <aliameer1977@gmail.com>
2017-06-12 14:38:57 -0700

    News
    Get Headlines News From the Global

---
+++ scripts/Data.js
@@ -0,0 +1,154 @@
+﻿
+
+
+
+/************************************************General News Array*******************************************************************/
+var worldtUrl = [
+  {
+      url: "https://newsapi.org/v1/articles?source=abc-news-au&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+      id: "#A"
+  },
+   {
+       url: "https://newsapi.org/v1/articles?source=associated-press&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+       id: "#B"
+   },
+{
+    url: "https://newsapi.org/v1/articles?source=ars-technica&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+    id: "#C"
+},
+ {
+     url: "https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+     id: "#D"
+ },
+  {
+      url: "https://newsapi.org/v1/articles?source=cnbc&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+      id: "#E"
+  },
+  {
+      url: "https://newsapi.org/v1/articles?source=reuters&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+      id: "#F"
+  }, {
+      url: "https://newsapi.org/v1/articles?source=al-jazeera-english&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+      id: "#G"
+  },
+ 
+    {
+        url: "https://newsapi.org/v1/articles?source=independent&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+        id: "#H"
+    },
+  
+
+
+];
+/************************************************Sport Array*******************************************************************/
+var sportUrl = [
+  {
+      url: "https://newsapi.org/v1/articles?source=fox-sports&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+      id: "#A"
+  },
+   {
+       url: "https://newsapi.org/v1/articles?source=bbc-sport&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+       id: "#B"
+   },
+{
+    url: "https://newsapi.org/v1/articles?source=espn&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+    id: "#C"
+},
+ {
+     url: "https://newsapi.org/v1/articles?source=four-four-two&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+     id: "#D"
+ },
+  {
+      url: "https://newsapi.org/v1/articles?source=nfl-news&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+      id: "#E"
+  },
+  {
+      url: "https://newsapi.org/v1/articles?source=the-sport-bible&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+      id: "#F"
+  },
+  {
+      url: "https://newsapi.org/v1/articles?source=sky-sports-news&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+      id: "#G"
+  },
+    {
+        url: "https://newsapi.org/v1/articles?source=talksport&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+        id: "#H"
+    },
+   
+
+];
+
+/************************************************Business Array*******************************************************************/
+
+var BusinessUrl  = [
+  {
+      url: "https://newsapi.org/v1/articles?source=handelsblatt&sortBy=latest&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+      id: "#A"
+  },
+   {
+       url: " https://newsapi.org/v1/articles?source=wirtschafts-woche&sortBy=latest&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+       id: "#B"
+   },
+{
+    url: " https://newsapi.org/v1/articles?source=fortune&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+    id: "#C"
+},
+ {
+     url: "https://newsapi.org/v1/articles?source=business-insider&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+     id: "#D"
+ },
+  {
+      url: "https://newsapi.org/v1/articles?source=financial-times&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+      id: "#E"
+  },
+  {
+      url: "https://newsapi.org/v1/articles?source=fortune&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+      id: "#F"
+  },
+  {
+      url: " https://newsapi.org/v1/articles?source=the-economist&sortBy=latest&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+      id: "#G"
+  },
+    {
+        url: " https://newsapi.org/v1/articles?source=the-wall-street-journal&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+        id: "#H"
+    },
+  
+  /************************************************Science Array*******************************************************************/
+];
+var ScienceUrl = [
+  {
+      url: "https://newsapi.org/v1/articles?source=ars-technica&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+      id: "#A"
+  },
+   {
+       url: "  https://newsapi.org/v1/articles?source=engadget&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+       id: "#B"
+   },
+{
+    url: " https://newsapi.org/v1/articles?source=gruenderszene&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+    id: "#C"
+},
+ {
+     url: "https://newsapi.org/v1/articles?source=hacker-news&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+     id: "#D"
+ },
+  {
+      url: "https://newsapi.org/v1/articles?source=national-geographic&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+      id: "#E"
+  },
+  {
+      url: "https://newsapi.org/v1/articles?source=new-scientist&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+      id: "#F"
+  },
+  {
+      url: " https://newsapi.org/v1/articles?source=recode&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+      id: "#G"
+  },
+    {
+        url: " https://newsapi.org/v1/articles?source=the-verge&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+        id: "#H"
+    },
+
+
+];
---
+++ scripts/Js.js
@@ -0,0 +1,100 @@
+﻿var scrollbar = [];
+$(document).ready(function () {
+
+    function getData(url, id1) {
+        $.get(url, function (data) {
+
+            var news = "";
+            for (var i = 0; i < 1; i++) {
+                var sub = data.articles[i].description;
+                if (sub.length > 150) {
+                    var res = sub.slice(0, 150);
+
+                }
+                else if (sub.length <150 && sub.length>120) {
+
+                    var res = sub +"<br>";
+                }
+                else {
+                    
+                    res = sub+"<br><br>";
+                }
+              
+
+                var source = data.source.toUpperCase();
+               
+                news += '<div id="main">'; ' "alt="Card image cap">'
+                news += "<a href='" + data.articles[i].url + "'>";
+                news += '<img class="card-img-top" src='+"'"+ data.articles[i].urlToImage +"'" + "alt="+"image"+"-"+ data.source + ">" ;
+                news += "</a>";
+                news += ' <div class="card-block">';
+                news += "<p class='Source'>"  + source + "</p>";
+                news += '  <h4 class="card-title">' + data.articles[i].title + '</h4>';
+                news += '<p class="card-text">' + res + '<span style="margin-left:10px">' + "<a href='" + data.articles[i].url + "'><br>Read More...</a>" + '</p></span>';
+               
+                news += "<span class='Source'>"+ "<p> Published At:"  + data.articles[i].publishedAt + "</span>" + "</p>";
+                
+                news += '</div>';
+                news += '</div>';
+               
+            
+                scrollbar += '<span class="scroll">'+source+ ":" + data.articles[i].title + ".</span>";
+                marquee();
+              }   
+           
+            function marquee() {
+                var myscroll = "";
+                for (var i = 0; i < scrollbar.length; i++) {
+                    myscroll += scrollbar[i];
+
+                }
+                $('#scroll').html(scrollbar);
+            }
+            
+            $(id1).html(news);
+
+        });
+    }
+    
+    document.getElementById('sport').onclick = function () {
+        for (var i = 0; i < sportUrl.length; i++) {
+            getData(sportUrl[i].url, sportUrl[i].id);
+        }
+        document.getElementById("headline").innerHTML = "SPORT NEWS";
+      
+        document.getElementById("jumbotron").style.backgroundImage = " url('img/person-sport-bike.jpg')";
+        document.getElementById("jumbotron").style.color = "white";
+    }
+    
+    document.getElementById('business').onclick = function () {
+        for (var i = 0; i < BusinessUrl.length; i++) {
+            getData(BusinessUrl[i].url, BusinessUrl[i].id);
+        }
+        document.getElementById("headline").innerHTML = "BUSINESS NEWS";
+        document.getElementById("jumbotron").style.backgroundImage = " url('img/Business.jpg')";
+        document.getElementById("scroll").style.color = "white";
+        
+    }
+    document.getElementById('science').onclick = function () {
+        for (var i = 0; i < ScienceUrl.length; i++) {
+            getData(ScienceUrl[i].url, ScienceUrl[i].id);
+        }
+        document.getElementById("headline").innerHTML = "SCIENCE NEWS";
+        document.getElementById("jumbotron").style.backgroundImage = " url('img/science.png')";
+        
+    }
+
+   
+    home();
+        function home() {
+        for (var i = 0; i < worldtUrl.length; i++) {
+            getData(worldtUrl[i].url, worldtUrl[i].id);
+
+        }
+        }    
+      });
+
+   
+   
+    
+
