eaa0b1ee1ec423636921d5bc9f5b809f92fbbe12
Ismael <aliameer1977@gmail.com>
2017-07-28 18:15:56 -0700

    Update Js.js

--- scripts/Js.js
+++ scripts/Js.js
@@ -4,25 +4,9 @@ $(document).ready(function () {
     function getData(url, id1) {
         $.get(url, function (data) {
 
-            var news = "";
-            for (var i = 0; i < 1; i++) {
-                var sub = data.articles[i].description;
-                if (sub.length > 150) {
-                    var res = sub.slice(0, 150);
-
-                }
-                else if (sub.length < 200 && sub.length > 120) {
-
-                    var res = sub + "<br>";
-                }
-                else {
-
-                    res = sub + "<br><br>";
-                }
-
-
+            var news = "";            
                 var source = data.source.toUpperCase();
-                for (var i = 0; i < 9; i++) {
+                for (var i = 0; i < 8; i++) {
 
                         
                         news += '<div class="main">'; ' "alt="Card image cap">'
@@ -33,7 +17,19 @@ $(document).ready(function () {
                         news += "<p id='SourceText' class='Source'>" + source + "</p>";
 
                         news += '  <span class="card-title">' + data.articles[i].title + '</span> <br><br>';
-                        news += '<p class="card-text ">' + res + '</p> ';
+                        
+
+                        var sub = data.articles[i].description;
+                        if (sub.length > 150) {
+                            var res = sub.slice(0, 150);
+
+                        }
+                        else if (sub.length < 150) {
+
+                            var res = sub + "<br><br";
+                        }
+
+                        news += '<p class="card-text">' + res+ '</p> ';
                         news += '<span  style="margin-left:10px">' + "<a class='hidden-md-down'  href='" + data.articles[i].url + "'><br><span style='color:blue'>More details...</span></a>" + '</span>';
                         news += "</a>";
 
@@ -41,31 +37,31 @@ $(document).ready(function () {
 
                         news += '</div>';
                         news += '</div>';
-
+                        }
                     
 
                    
-                }
+                
 
                 scrollbar += '<span class="scroll">' + source + ":" + data.articles[i].title + ".</span>";
                 marquee();
-            }
+            
 
-            function marquee() {
-                var myscroll = "";
-                for (var i = 0; i < scrollbar.length; i++) {
-                    myscroll += scrollbar[i];
+                function marquee() {
+                    var myscroll = "";
+                    for (var i = 0; i < scrollbar.length; i++) {
+                        myscroll += scrollbar[i];
+
+                    }
+                    $('#scroll').html(scrollbar);
+                    $('#scrollSmallDevices').html(scrollbar);
 
                 }
-                $('#scroll').html(scrollbar);
-                $('#scrollSmallDevices').html(scrollbar);
-               
-            }
 
-            $(id1).html(news);
+                $(id1).html(news);
 
-        });
-    }
+                });
+        }
 
     document.getElementById('sport').onclick = function () {
         for (var i = 0; i < sportUrl.length; i++) {
@@ -78,6 +74,9 @@ $(document).ready(function () {
         document.getElementById("jumbotron").style.color = "white";
     }
 
+
+
+
     document.getElementById('business').onclick = function () {
         for (var i = 0; i < BusinessUrl.length; i++) {
             getData(BusinessUrl[i].url, BusinessUrl[i].id);
@@ -88,9 +87,17 @@ $(document).ready(function () {
         document.getElementById("scroll").style.color = "white";
 
     }
+
+
+
+
     document.getElementById('science').onclick = function () {
         for (var i = 0; i < ScienceUrl.length; i++) {
+           
+        
             getData(ScienceUrl[i].url, ScienceUrl[i].id);
+            
+            
         }
         document.getElementById("headline").innerHTML = "SCIENCE NEWS";
         document.getElementById("worldNewsSmall").innerHTML = "SCIENCE NEWS";
@@ -99,14 +106,24 @@ $(document).ready(function () {
     }
 
 
+   
+
     home();
+    
     function home() {
+       
         for (var i = 0; i < worldtUrl.length; i++) {
             getData(worldtUrl[i].url, worldtUrl[i].id);
 
         }
     }
 });
+
+
+
+
+
+
 
 
 
