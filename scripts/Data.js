4b2f27f59a45f0ea6f3a7947d5ac9e92b9fc6165
Ismael <aliameer1977@gmail.com>
2017-07-30 09:14:42 -0700

    update business news links

--- scripts/Data.js
+++ scripts/Data.js
@@ -87,7 +87,7 @@ var BusinessUrl  = [
       id: "#A"
   },
    {
-       url: "https://newsapi.org/v1/articles?source=business-insider&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+       url: " https://newsapi.org/v1/articles?source=financial-times&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
        id: "#B"
    },
 {
@@ -103,7 +103,7 @@ var BusinessUrl  = [
       id: "#E"
   },
   {
-      url: "https://newsapi.org/v1/articles?source=fortune&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
+      url: "https://newsapi.org/v1/articles?source=usa-today&sortBy=top&apiKey=7dd784debc8a4821bfefac631dbd3d53",
       id: "#F"
   },
   {
